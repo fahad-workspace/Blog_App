019909fdfa4007795b851136287e7bb80e8d3ae6
Fahad Sarwar <fahad.workspace@outlook.com>
2015-03-13 12:40:57 +0530

    Added CRUD operation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,25 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "fahad", password: "password", except: [:index, :show]
+
   def new
   end
   
   def index
     @articles = Article.all
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -14,6 +29,12 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
   
   def show
